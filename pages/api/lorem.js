297557cd96c90613870e0d788988ac25f50d6a7f
magma <magmafellow@gmail.com>
2023-08-10 15:52:54 +0300

    changed lorem api

--- pages/api/lorem.js
+++ pages/api/lorem.js
@@ -2,6 +2,6 @@ export default async function handler(req, res) {
 	const response = await fetch(
 		'https://baconipsum.com/api/?type=meat-and-filler'
 	)
-	const lorem = response.json()
+	const lorem = await response.json()
 	res.status(200).json({ data: lorem })
 }
