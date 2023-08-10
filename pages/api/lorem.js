817a159e14068aa3c4c356a385657ebc377c5541
magma <magmafellow@gmail.com>
2023-08-10 15:52:18 +0300

    repaired lorem api. v2

--- pages/api/lorem.js
+++ pages/api/lorem.js
@@ -3,5 +3,5 @@ export default async function handler(req, res) {
 		'https://baconipsum.com/api/?type=meat-and-filler'
 	)
 	const lorem = response.json()
-	res.status(200).json({ data: answer })
+	res.status(200).json({ data: lorem })
 }
