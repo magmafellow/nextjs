eef324cc096616882abe984a148236dade2b13ec
magma <magmafellow@gmail.com>
2023-08-10 15:51:42 +0300

    repaired lorem api

--- pages/api/lorem.js
+++ pages/api/lorem.js
@@ -1,4 +1,7 @@
 export default async function handler(req, res) {
-	const answer = await fetch('https://baconipsum.com/api/?type=meat-and-filler')
+	const response = await fetch(
+		'https://baconipsum.com/api/?type=meat-and-filler'
+	)
+	const lorem = response.json()
 	res.status(200).json({ data: answer })
 }
